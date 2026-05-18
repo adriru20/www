4d768cdff191a400337814be45cd9dcb1f5e7282
Adriru20 <124593111+adriru20@users.noreply.github.com>
2026-05-18 16:51:37 +0200

    2026-05-18_update-homepage

--- index.php
+++ index.php
@@ -13,8 +13,11 @@ if (!isset($_SESSION['user_id'])) {
   <?php include "{$src}frontend/menu.php"; ?>
   <nav class="container">
 
-    <h1>Hola1</h1>
-    <h1>Hola2</h1>
+    <li><a href="https://<?=$url?>/"><b>INICIO</b></a></li>
+    <li><a href="https://<?=$url?>/apps/wiki/"><b>WIKI</b></a></li>
+    <li><a href="https://<?=$url?>/apps/parking/"><b>PARKING</b></a></li>
+    <li><a href="https://<?=$url?>/apps/giftlist/"><b>GIFT LIST</b></a></li>
+    <li><a href="https://<?=$url?>/apps/inventario/"><b>INVENTARIO</b></a></li>
 
   </nav>
   <?php include "{$src}frontend/footer.php"; ?>
