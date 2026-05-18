2b919a17c3d894bed853147caec2daebab7d5921
Adriru20 <124593111+adriru20@users.noreply.github.com>
2026-05-18 17:00:08 +0200

    2026-05-18_update-homepage

--- index.php
+++ index.php
@@ -12,13 +12,11 @@ if (!isset($_SESSION['user_id'])) {
 <body>
   <?php include "{$src}frontend/menu.php"; ?>
   <nav class="container">
-
-    <li><a href="https://<?=$url?>/"><b>INICIO</b></a></li>
-    <li><a href="https://<?=$url?>/apps/wiki/"><b>WIKI</b></a></li>
-    <li><a href="https://<?=$url?>/apps/parking/"><b>PARKING</b></a></li>
-    <li><a href="https://<?=$url?>/apps/giftlist/"><b>GIFT LIST</b></a></li>
-    <li><a href="https://<?=$url?>/apps/inventario/"><b>INVENTARIO</b></a></li>
-
+    <li><a href="https://<?=$url?>/" class="btn btn-primary"><b>INICIO</b></a></li>
+    <li><a href="https://<?=$url?>/apps/wiki/" class="btn btn-primary"><b>WIKI</b></a></li>
+    <li><a href="https://<?=$url?>/apps/parking/" class="btn btn-primary"><b>PARKING</b></a></li>
+    <li><a href="https://<?=$url?>/apps/giftlist/" class="btn btn-primary"><b>GIFT LIST</b></a></li>
+    <li><a href="https://<?=$url?>/apps/inventario/" class="btn btn-primary"><b>INVENTARIO</b></a></li>
   </nav>
   <?php include "{$src}frontend/footer.php"; ?>
 </body>
