5fd6298275a16e419e2d4bd3b371abdd4b4704e3
Adriru20 <124593111+adriru20@users.noreply.github.com>
2026-05-19 19:35:09 +0200

    .

--- frontend/head.php
+++ frontend/head.php
@@ -1,9 +1,10 @@
 <head>
   <meta charset="UTF-8"/>
-  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-  <link rel="manifest" href="../backend/PWA/manifest.json">
+  <link rel="manifest" href="<?=$src?>backend/PWA/manifest.json">
   <meta name="theme-color" content="#212529">
   <link rel="apple-touch-icon" href="<?=$src?>img/Astronauta-Icono.jpg">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
   <link rel="shortcut icon" href="<?=$src?>img/favicon.ico" type="image/x-icon"/>
   <link rel="stylesheet" href="<?=$src?>styles/style.css"/>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous"/>
