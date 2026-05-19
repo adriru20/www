d4c3d244c930ecf406f59269339fbebc81bdf5b6
Adriru20 <124593111+adriru20@users.noreply.github.com>
2026-05-19 19:33:47 +0200

    .

--- backend/PWA/sw.js
+++ backend/PWA/sw.js
@@ -1,6 +1,6 @@
 const CACHE_NAME = 'inventario-cache-v1';
 const urlsToCache = [
-  '../../apps/inventario/',
+  '../../apps/inventario/index.php',
   '../../js/index.js'
   // Aquí podrías añadir tu CSS, Bootstrap o logos estáticos si los tienes
 ];
